43a57f2f9aaf96da2e1391da32dc91e63da33886
이민우 <aksenrjDeveloper@gmail.com>
2017-11-01 16:57:14 +0900

    빈 프로젝트로 변경

---
+++ xapps/xappsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace xapps
+{
+    public partial class xappsPage : ContentPage
+    {
+        public xappsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
