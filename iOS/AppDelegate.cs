85873cf1f570745a50fee2e6f3c4dc267baae258
임한별 <coollhb@ubivelox.com>
2017-11-03 18:16:10 +0900

    [xamarin][임한별] FCM 연동 환경 적용( 인증서 확인필요 )

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -6,19 +6,148 @@ using Foundation;
 using UIKit;
 
 using Firebase;
+using Firebase.CloudMessaging;
+using Firebase.InstanceID;
+
+using UserNotifications;
 
 namespace xapps.iOS
 {
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            Console.WriteLine("FinishedLaunching");
             global::Xamarin.Forms.Forms.Init();
 
             LoadApplication(new App());
 
+            // get permission for notification
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+            }
+            else
+            {
+                // iOS 9 <=
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            // Firebase component initialize
+            //Firebase.Analytics.App.Configure();
+            Firebase.Core.App.Configure();
+
+            Firebase.InstanceID.InstanceId.Notifications.ObserveTokenRefresh((sender, e) => {
+                var newToken = Firebase.InstanceID.InstanceId.SharedInstance.Token;
+                // if you want to send notification per user, use this token
+                System.Diagnostics.Debug.WriteLine(newToken);
+
+                connectFCM();
+            });
+
             return base.FinishedLaunching(app, options);
+        }
+
+        public override void DidEnterBackground(UIApplication uiApplication)
+        {
+            Messaging.SharedInstance.Disconnect();
+            Console.WriteLine("Disconnected from FCM");
+        }
+
+        public override void OnActivated(UIApplication uiApplication)
+        {
+            connectFCM();
+            base.OnActivated(uiApplication);
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+#if DEBUG
+            Firebase.InstanceID.InstanceId.SharedInstance.SetApnsToken(deviceToken, Firebase.InstanceID.ApnsTokenType.Sandbox);
+#endif
+#if RELEASE
+            Firebase.InstanceID.InstanceId.SharedInstance.SetApnsToken(deviceToken, Firebase.InstanceID.ApnsTokenType.Prod);
+#endif
+        }
+
+        // iOS 9 <=, fire when recieve notification foreground
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            Messaging.SharedInstance.AppDidReceiveMessage(userInfo);
+
+            // Generate custom event
+            NSString[] keys = { new NSString("Event_type") };
+            NSObject[] values = { new NSString("Recieve_Notification") };
+            var parameters = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(keys, values, keys.Length);
+
+            // Send custom event
+            Firebase.Analytics.Analytics.LogEvent("CustomEvent", parameters);
+
+            if (application.ApplicationState == UIApplicationState.Active)
+            {
+                System.Diagnostics.Debug.WriteLine(userInfo);
+                var aps_d = userInfo["aps"] as NSDictionary;
+                var alert_d = aps_d["alert"] as NSDictionary;
+                var body = alert_d["body"] as NSString;
+                var title = alert_d["title"] as NSString;
+                debugAlert(title, body);
+            }
+        }
+
+        [Export ("userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:")]
+        public void DidReceibeNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler) {
+            System.Console.WriteLine("DidReceibeNotificationResponse");
+        }
+
+        // iOS 10, fire when recieve notification foreground
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            var title = notification.Request.Content.Title;
+            var body = notification.Request.Content.Body;
+            debugAlert(title, body);
+        }
+
+        // Receive data message on iOS 10 devices.
+        public void ApplicationReceivedRemoteMessage(RemoteMessage remoteMessage)
+        {
+            Console.WriteLine(remoteMessage.AppData);
+        }
+
+        private void connectFCM()
+        {
+            Console.WriteLine("connected from FCM");
+            Messaging.SharedInstance.Connect((error) =>
+            {
+                if (error == null)
+                {
+                    Messaging.SharedInstance.Subscribe("/topics/all");
+                }
+                System.Diagnostics.Debug.WriteLine(error != null ? "error occured" : "connect success");
+
+                var token = InstanceId.SharedInstance.Token;
+                Console.WriteLine("token = " + token);
+
+            });
+        }
+
+        private void debugAlert(string title, string message)
+        {
+            var alert = new UIAlertView(title ?? "Title", message ?? "Message", null, "Cancel", "OK");
+            alert.Show();
         }
     }
 }
