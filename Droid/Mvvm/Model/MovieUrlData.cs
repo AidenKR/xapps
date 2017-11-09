2c518daaf5b6365842b0c538f298800e73bf8ef5
shyook <shyook@ubivelox.com>
2017-11-09 13:25:53 +0900

    [xamarin][shyook]미리보기 전체 화면 - 미리보기 URL Native에 전달 하도록 수정

---
+++ Droid/Mvvm/Model/MovieUrlData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using xapps;
+
+[assembly: Dependency(typeof(MovieUrlData))]
+namespace xapps
+{
+    class MovieUrlData : IMovieUrl
+    {
+        public static string previewUrl { get; set; }
+        public void MovieUrl(string url)
+        {
+            previewUrl = url;
+        }
+    }
+}
---
+++ iOS/Mvvm/Services/Helper/DeviceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace xapps
+{
+    //Create statice device helper to capture the current device. This will be used to detect screen orientation
+    public static class DeviceHelper
+    {
+        public static UIDevice iOSDevice
+        {
+            get;
+            set;
+        }
+    }
+}
