fbd7f6568594c2571c578d38089b4cfc53460315
shyook <shyook@ubivelox.com>
2017-11-13 09:52:04 +0900

    [xamarin][shyook] iOS 런처 스크린

---
+++ iOS/Mvvm/Model/MovieUrlData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using xapps;
+
+[assembly: Dependency(typeof(MovieUrlData))]
+namespace xapps
+{
+    class MovieUrlData : IMovieUrl
+    {
+        public static string previewUrl { get; set; }
+
+        public void MovieUrl(string url)
+        {
+            previewUrl = url;
+        }
+    }
+}
