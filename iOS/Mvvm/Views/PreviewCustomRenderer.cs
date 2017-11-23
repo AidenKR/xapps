82abc9e00352f40d736a6327f78ccb3096adef48
임한별 <coollhb@ubivelox.com>
2017-11-23 17:07:38 +0900

    [xamarin][임한별] iOS 상세화면의 동영상 재생기능 동작 수정.

--- iOS/Mvvm/Views/PreviewCustomRenderer.cs
+++ iOS/Mvvm/Views/PreviewCustomRenderer.cs
@@ -18,7 +18,7 @@ using CoreMedia;
 [assembly: ExportRenderer(typeof(DetailPreview), typeof(PreviewCustomRenderer))]
 namespace xapps.iOS
 {
-    class PreviewCustomRenderer : ViewRenderer, IAVPictureInPictureControllerDelegate
+    class PreviewCustomRenderer : ViewRenderer
     {
         AVPlayer player;
         AVPlayerLayer playerLayer;
@@ -27,7 +27,7 @@ namespace xapps.iOS
 
         UIButton playButton;
         UIButton stopButton;
-        AVPlayerViewController controller;
+        bool isPlaying;
 
         protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
@@ -42,30 +42,46 @@ namespace xapps.iOS
 
             SetNativeControl(new UIView());
 
-            Debug.WriteLine("OnElementChanged");
+            Console.WriteLine("OnElementChanged");
 
             asset = AVAsset.FromUrl(new NSUrl(MovieUrlData.previewUrl));
             playerItem = new AVPlayerItem(asset);
 
             player = new AVPlayer(playerItem);
-            player.AddObserver(this, (NSString)"status", NSKeyValueObservingOptions.New, IntPtr.Zero);
-
-            controller = new AVPlayerViewController();
-            controller.Player = player;
             playerLayer = AVPlayerLayer.FromPlayer(player);
+            isPlaying = false;
+        }
+
+        protected override void Dispose(bool disposing) {
+            base.Dispose(disposing);
+
+            Console.WriteLine("Dispose disposing = " + disposing);
+            Console.WriteLine("playerLayer.ReadyForDisplay = " + playerLayer.ReadyForDisplay);
+            if(disposing && isPlaying) {
+                player.Pause();
+            }
         }
 
         public override void LayoutSubviews()
         {
             base.LayoutSubviews();
 
-            Debug.WriteLine("LayoutSubviews");
+            Console.WriteLine("LayoutSubviews");
 
             playerLayer = AVPlayerLayer.FromPlayer(player);
             playerLayer.VideoGravity = AVLayerVideoGravity.ResizeAspectFill;
             playerLayer.NeedsDisplayOnBoundsChange = true;
             playerLayer.Frame = NativeView.Bounds;
             NativeView.Layer.AddSublayer(playerLayer);
+
+            player.AddObserver(this, (NSString)"status", NSKeyValueObservingOptions.New, IntPtr.Zero);
+            player.AddBoundaryTimeObserver(times: new[] { NSValue.FromCMTime(new CMTime(playerItem.Asset.Duration.Value, playerItem.Asset.Duration.TimeScale)) },
+                                            queue: null,
+                                            handler: () =>
+                                            {
+                                                player.Seek(new CMTime(0, 0));
+                                                controlPlayer(true);
+                                            });
 
             CGRect rect = NativeView.Bounds;
             playButton = new UIButton(new CGRect((rect.Size.Width / 2) - 25, (rect.Size.Height / 2) - 25, 50, 50));
@@ -105,163 +121,45 @@ namespace xapps.iOS
             //}
         }
 
+        public void videoFinished(NSNotification notify)
+        {
+            Console.WriteLine("videoFinished");
+        }
+
         private void TouchUpInsidePlayEvent(object sender, EventArgs e)
         {
             Console.WriteLine("TouchUpInsidePlayEvent");
 
-            player.Play();
-
-            playButton.Hidden = true;
-            stopButton.Hidden = false;
+            controlPlayer(false);
         }
 
         private void TouchUpInsideStopEvent(object sender, EventArgs e)
         {
             Console.WriteLine("TouchUpInsideStopEvent");
 
-            player.Pause();
-
-            playButton.Hidden = false;
-            stopButton.Hidden = true;
+            controlPlayer(true);
         }
 
-        [Export("pictureInPictureControllerDidStartPictureInPicture:")]
-        public void DidStartPictureInPicture(AVPictureInPictureController pictureInPictureController)
+        private void controlPlayer(bool stoping)
         {
-            Console.WriteLine("DidStartPictureInPicture");
-            //PictureInPictureActiveLabel.Hidden = false;
-            //Toolbar.Hidden = true;
-        }
+            if (stoping)
+            {
+                player.Pause();
+            }
+            else
+            {
+                player.Play();
 
-        [Export("pictureInPictureControllerWillStopPictureInPicture:")]
-        public void WillStopPictureInPicture(AVPictureInPictureController pictureInPictureController)
-        {
-            Console.WriteLine("WillStopPictureInPicture");
-            //PictureInPictureActiveLabel.Hidden = true;
-            //Toolbar.Hidden = false;
-        }
+            }
 
-        [Export("pictureInPictureControllerFailedToStartPictureInPicture:withError:")]
-        public void FailedToStartPictureInPicture(AVPictureInPictureController pictureInPictureController, NSError error)
-        {
-            Console.WriteLine("FailedToStartPictureInPicture");
-            //PictureInPictureActiveLabel.Hidden = true;
-            //Toolbar.Hidden = false;
-            //HandleError(error);
-        }
-
-        void ObserveCurrentItemDuration(NSObservedChange change)
-        {
-            Console.WriteLine("ObserveCurrentItemDuration");
-            //CMTime newDuration;
-            //var newDurationAsValue = change.NewValue as NSValue;
-            //newDuration = (newDurationAsValue != null) ? newDurationAsValue.CMTimeValue : CMTime.Zero;
-            //var hasValidDuration = newDuration.IsNumeric && newDuration.Value != 0;
-            //var newDurationSeconds = hasValidDuration ? newDuration.Seconds : 0;
-
-            //TimeSlider.MaxValue = (float)newDurationSeconds;
-
-            //var currentTime = Player.CurrentTime.Seconds;
-            //TimeSlider.Value = (float)(hasValidDuration ? currentTime : 0);
-
-            //PlayPauseButton.Enabled = hasValidDuration;
-            //TimeSlider.Enabled = hasValidDuration;
-        }
-
-        void ObserveCurrentRate(NSObservedChange change)
-        {
-            Console.WriteLine("ObserveCurrentRate");
-            //// Update playPauseButton type.
-            //var newRate = ((NSNumber)change.NewValue).DoubleValue;
-
-            //UIBarButtonSystemItem style = (Math.Abs(newRate) < float.Epsilon) ? UIBarButtonSystemItem.Play : UIBarButtonSystemItem.Pause;
-            //var newPlayPauseButton = new UIBarButtonItem(style, PlayPauseButtonWasPressed);
-
-            //// Replace the current button with the updated button in the toolbar.
-            //UIBarButtonItem[] items = Toolbar.Items;
-
-            //var playPauseItemIndex = Array.IndexOf(items, PlayPauseButton);
-            //if (playPauseItemIndex >= 0)
-            //{
-            //    items[playPauseItemIndex] = newPlayPauseButton;
-            //    PlayPauseButton = newPlayPauseButton;
-            //    Toolbar.SetItems(items, false);
-            //}
-        }
-
-        void ObserveCurrentItemStatus(NSObservedChange change)
-        {
-            Console.WriteLine("ObserveCurrentItemStatus");
-            //// Display an error if status becomes Failed
-            //var newStatusAsNumber = change.NewValue as NSNumber;
-            //AVPlayerItemStatus newStatus = (newStatusAsNumber != null)
-            //    ? (AVPlayerItemStatus)newStatusAsNumber.Int32Value
-            //    : AVPlayerItemStatus.Unknown;
-
-            //if (newStatus == AVPlayerItemStatus.Failed)
-            //{
-            //    HandleError(Player.CurrentItem.Error);
-            //}
-            //else if (newStatus == AVPlayerItemStatus.ReadyToPlay)
-            //{
-            //    var asset = Player.CurrentItem != null ? Player.CurrentItem.Asset : null;
-            //    if (asset != null)
-            //    {
-
-            //        // First test whether the values of `assetKeysRequiredToPlay` we need
-            //        // have been successfully loaded.
-            //        foreach (var key in assetKeysRequiredToPlay)
-            //        {
-            //            NSError error;
-            //            if (asset.StatusOfValue(key, out error) == AVKeyValueStatus.Failed)
-            //            {
-            //                HandleError(error);
-            //                return;
-            //            }
-            //        }
-
-            //        if (!asset.Playable || asset.ProtectedContent)
-            //        {
-            //            // We can't play this asset.
-            //            HandleError(null);
-            //            return;
-            //        }
-
-            //        // The player item is ready to play,
-            //        // setup picture in picture.
-            //        SetupPictureInPicturePlayback();
-            //    }
-            //}
+            isPlaying = !stoping;
+            playButton.Hidden = !stoping;
+            stopButton.Hidden = stoping;
         }
 
         public override void ObserveValue(NSString keyPath, NSObject ofObject, NSDictionary change, IntPtr context)
         {
             Console.WriteLine("ObserveValue");
-            //if (context != IntPtr.Zero)
-            //{
-            //    base.ObserveValue(keyPath, ofObject, change, context);
-            //    return;
-            //}
-
-            //var ch = new NSObservedChange(change);
-
-            //if (keyPath == "pictureInPicturePossible")
-            //{
-            //    // Enable the `PictureInPictureButton` only if `PictureInPicturePossible`
-            //    // is true. If this returns false, it might mean that the application
-            //    // was not configured as shown in the AppDelegate.
-            //    PictureInPictureButton.Enabled = ((NSNumber)ch.NewValue).BoolValue;
-            //}
-        }
-
-        void HandleError(NSError error)
-        {
-            Console.WriteLine("HandleError");
-            //string message = error != null ? error.LocalizedDescription : string.Empty;
-            //var alertController = UIAlertController.Create("Error", message, UIAlertControllerStyle.Alert);
-            //var alertAction = UIAlertAction.Create("OK", UIAlertActionStyle.Default, null);
-            //alertController.AddAction(alertAction);
-            //PresentViewController(alertController, true, null);
         }
     }
 }
